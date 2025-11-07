b05a15dc2f59f22dd586a48e6eeac607d4293fa5
Vitor Vianna <vmonteiro2004@gmail.com>
2025-11-07 20:06:15 -0300

    feat: adiciona suporte ao usuário no ChatService e atualiza a lógica de envio de mensagens

--- app/chat/chat.py
+++ app/chat/chat.py
@@ -1,19 +1,37 @@
 from datetime import datetime, timezone
 from typing import Optional
 
+from app.shared import utils
+
+from app.users.models import User
+
 from app.chat.messages.user_message import UserMessage
 from app.chat.messages.genai_message import GenaiMessage
 from app.chat.repository import ChatRepository
 from app.genai.handler import GenaiHander
-from app.shared import utils
+
+from app.chat.runners.embedding import EmbeddingRunner
 
 
 class ChatService:
-    def __init__(self, repository: ChatRepository, handler: GenaiHander):
+    def __init__(
+            self, 
+            repository: ChatRepository, 
+            handler: GenaiHander,
+            user: User
+    ):
         self._repository = repository
         self._handler = handler
+        self.user = user
 
     async def send_message(self, user_id: str, payload) -> dict:
+
+        if payload.use_embedding:
+            emb_runner = EmbeddingRunner(user= self.user)
+            payload.prompt = emb_runner.enrich_prompt(
+                prompt=payload.prompt,
+            )
+    
         conversation_id = payload.chat_id or utils.generate_hash_id()
         conversation = await self._repository.get_conversation(
             user_id=user_id, conversation_id=conversation_id
